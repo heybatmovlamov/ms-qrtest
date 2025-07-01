01415198c79832d5bcca069eae5034a6560e9b84
Yusif <yusif.asadli2003@gmail.com>
2025-07-01 16:03:26 +0400

    recaptcha

--- src/main/java/com/msqrtets/controller/QrController.java
+++ src/main/java/com/msqrtets/controller/QrController.java
@@ -1,21 +1,24 @@
 package com.msqrtets.controller;
 
 import com.msqrtets.service.QrGenerateService;
+import com.msqrtets.service.RecaptchaService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/test")
 public class QrController {
 
-    private final QrGenerateService service;
+    private final QrGenerateService qrService;
+    private final RecaptchaService recaptchaService;
 
     @GetMapping("/qr/{name}")
-    public String qr(@PathVariable String name) {
-        return service.generateQRCode(name);
+    public String qr(@PathVariable String name, @RequestParam("recaptchaToken") String recaptchaToken) {
+        boolean valid = recaptchaService.verify(recaptchaToken);
+        if (!valid) {
+            return "Captcha yoxlanışı uğursuz oldu.";
+        }
+        return qrService.generateQRCode(name);
     }
 }
