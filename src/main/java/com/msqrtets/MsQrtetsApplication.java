1ccb14160dff30443eb9e26de4fcf66abd0c98cb
Heybat Movlamov <heybatmovlamov@gmail.com>
2025-07-01 12:59:33 +0400

    initial commit

---
+++ src/main/java/com/msqrtets/MsQrtetsApplication.java
@@ -0,0 +1,13 @@
+package com.msqrtets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsQrtetsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsQrtetsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/msqrtets/MsQrtetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msqrtets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsQrtetsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
