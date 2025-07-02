687b2d1a08096442563bf1c1ac21c2b2e5080271
Yusif <yusif.asadli2003@gmail.com>
2025-07-02 11:41:47 +0400

    asdf

--- src/main/java/com/msqrtets/service/Impl/UserServiceImpls.java
+++ src/main/java/com/msqrtets/service/Impl/UserServiceImpls.java
@@ -4,5 +4,5 @@ import com.msqrtets.service.UserService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class USerServiceImpls implements UserService {
+public class UserServiceImpls implements UserService {
 }
