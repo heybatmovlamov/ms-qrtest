d1ede76117e47eb96b8fd7fb250b0101e5767585
Yusif <yusif.asadli2003@gmail.com>
2025-07-02 11:24:37 +0400

    order and user

---
+++ src/main/java/com/msqrtets/model/Order.java
@@ -0,0 +1,26 @@
+package com.msqrtets.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String orderName;
+    private LocalDateTime orderDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id") // Order cədvəlində foreign key olacaq
+    private User user;
+
+
+}
--- src/main/java/com/msqrtets/model/TestUrl.java
+++
@@ -1,9 +0,0 @@
-package com.msqrtets.model;
-
-import jdk.jfr.Enabled;
-import lombok.Data;
-
-@Data
-public class TestUrl {
-    private String url;
-}
